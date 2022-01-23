fce01aea2a1624dbf29c0f0ca4a350440cd8d808
rahul <rsinghbaghel10@gmail.com>
2022-01-23 22:46:26 +0530

    now

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(checkIfInputIsAnEvenNumber(122));
+		// Testing in the main method
+	}
+	public static boolean checkIfInputIsAnEvenNumber(int
+			number){
+		return number % 2 == 0;
+	}
+
+	}
+
+
---
+++ src/test/java/TestMain.java
@@ -0,0 +1,13 @@
+
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestMain {
+	@Test
+	public void testInputIsEven(){
+		assertTrue(Main.checkIfInputIsAnEvenNumber(22)); //Assertion
+	}
+}
+
